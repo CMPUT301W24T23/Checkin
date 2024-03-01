e60b77f5c1aac9b6254980f04fb78ca37caa9aa5
Samira Salman <samirasalman24@gmail.com>
2024-02-29 21:46:28 -0700

    Added Home Page
    Added Home Page to Main Activity

--- app/src/main/java/com/example/checkin/MainActivity.java
+++ app/src/main/java/com/example/checkin/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.checkin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button organizerbutton;
+    Button attendeebutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        organizerbutton = findViewById(R.id.organizerbtn);
+        attendeebutton = findViewById(R.id.attendeebtn);
+
+
+
+        attendeebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // add new fragment
+
+            }
+        });
+
+        // Move to organizer screen when organizer button is clicked
+        organizerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // add new fragment 
+
+            }
+        });
+
+
     }
 }

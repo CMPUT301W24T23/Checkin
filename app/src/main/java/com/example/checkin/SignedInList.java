d97d2e66639a61cb0e893185dbb60604810ad295
Samira Salman <samirasalman24@gmail.com>
2024-03-07 21:15:59 -0700

    Add class

---
+++ app/src/main/java/com/example/checkin/SignedInList.java
@@ -0,0 +1,24 @@
+package com.example.checkin;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SignedInList extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_signed_in_list, container, false);
+
+        return view;
+    }
+}

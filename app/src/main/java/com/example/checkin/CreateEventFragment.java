50fdca9a807c923b8a1983be2c7784c6587829fa
Reem Rashid <reemsrashid8@gmail.com>
2024-03-28 09:10:51 -0600

    .

--- app/src/main/java/com/example/checkin/CreateEventFragment.java
+++ app/src/main/java/com/example/checkin/CreateEventFragment.java
@@ -11,14 +11,20 @@
 package com.example.checkin;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
+import androidx.preference.PreferenceManager;
 
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,6 +34,12 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class CreateEventFragment extends Fragment {
 
     private CheckBox checkBoxGeoTracking;
@@ -35,10 +47,17 @@ public class CreateEventFragment extends Fragment {
     private EditText eventdetails;
     private ImageView ivEventPoster;
     private Button btnAddPoster;
-    private Button btnMap;
+    private Organizer organizer;
 
-    private Button btnCreateEvent;
+    Button backbutton;
 
+    private Button addeventbutton;
+
+    private Button qrcodebutton;
+
+    private EventList events;
+
+    Event event;
 
     private final ActivityResultLauncher<String> mGetContent = registerForActivityResult(
             new ActivityResultContracts.GetContent(),
@@ -61,7 +80,11 @@ public class CreateEventFragment extends Fragment {
         eventdetails = view.findViewById(R.id.etEventDetails);
         ivEventPoster = view.findViewById(R.id.ivEventPoster);
         btnAddPoster = view.findViewById(R.id.btnAddPoster);
+        addeventbutton = view.findViewById(R.id.createeventbtn);
+        qrcodebutton = view.findViewById(R.id.btnGenerateQR);
+        backbutton = view.findViewById(R.id.backbtn);
 
+        Database database = new Database();
         btnAddPoster.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,45 +92,77 @@ public class CreateEventFragment extends Fragment {
             }
         });
 
-        btnMap = view.findViewById(R.id.btnMap);
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            events = (EventList) bundle.getSerializable("eventslist");
+        }
 
-        btnMap.setOnClickListener(new View.OnClickListener() {
+        backbutton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), MapActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button btnBack = view.findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
 
-        btnCreateEvent = view.findViewById(R.id.btnCreateEvent);
 
-        btnCreateEvent.setOnClickListener(new View.OnClickListener() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String android_id = preferences.getString("ID", "");
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference organizerRef = db.collection("Organizers").document(android_id);
+        organizerRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onClick(View v) {
-                createEvent();
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        // Convert the document snapshot to an Organizer object using Database class method
+                        organizer = database.getOrganizer(document);
+                        // Proceed with setting up the UI using the retrieved organizer object
+                    } else {
+                        Log.d("document", "No such document");
+                    }
+                } else {
+                    Log.d("error", "get failed with ", task.getException());
+                }
             }
         });
 
+
+
+        // choose event qr code to be generated
+        qrcodebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                qrcodebutton.setBackgroundColor(Color.GRAY);
+            }
+        });
+
+        // create new event and open list of events
+        addeventbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                event = new Event(eventname.getText().toString(), Settings.Secure.getString(getContext().getContentResolver(), Settings.Secure.ANDROID_ID));
+                //event.setEventname(eventname.getText().toString());
+                event.setEventdetails(eventdetails.getText().toString());
+                events.addEvent(event);
+                database.updateEvent(event);
+                organizer.EventCreate(event.getEventId());
+                database.updateOrganizer(organizer);
+                OrganizerFragment1 organizerfrag = new OrganizerFragment1();
+                Bundle args = new Bundle();
+                args.putSerializable("organizer", organizer);
+                args.putSerializable("eventslist", events);
+                organizerfrag.setArguments(args);
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.org_view, organizerfrag).addToBackStack(null).commit();
+
+            }
+        });
+
+
+
+
+
         return view;
-    }
-
-    private void createEvent() {
-        String eventName = eventname.getText().toString();
-        String eventDetails = eventdetails.getText().toString();
-        boolean isGeoTrackingEnabled = checkBoxGeoTracking.isChecked();
-
-        // Log the event details or save them to a database
-        Log.d("CreateEvent", "Event Name: " + eventName + ", Event Details: " + eventDetails + ", GeoTracking: " + isGeoTrackingEnabled);
-
-        // Navigate back to the previous fragment or activity
-        getActivity().getSupportFragmentManager().popBackStack();
     }
 }

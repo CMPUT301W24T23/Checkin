0ec67b3e30a759adfb63aff5eae86a05f6de13d7
Samira Salman <samirasalman24@gmail.com>
2024-04-04 00:37:57 -0600

    add fix

--- app/src/androidTest/java/com/example/checkin/AttendeeViewTest.java
+++ app/src/androidTest/java/com/example/checkin/AttendeeViewTest.java
@@ -10,6 +10,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
@@ -18,15 +19,22 @@ import static org.mockito.Mockito.when;
 
 import android.Manifest;
 import android.content.Context;
+import android.view.View;
 import android.widget.ListView;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
+import androidx.test.espresso.idling.CountingIdlingResource;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.GrantPermissionRule;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,18 +49,16 @@ import java.util.List;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 
-// need to comment out line 78 in Database class before testing
-// as it causes the app to close due to unique id generated not being set yet
 public class AttendeeViewTest {
 
     //@Mock
     //private Database mockDatabase;
 
     @Rule
-    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(Manifest.permission_group.CAMERA);
+    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(Manifest.permission.CAMERA);
 
     @Rule
-    public GrantPermissionRule permissionRule2 = GrantPermissionRule.grant(Manifest.permission_group.NOTIFICATIONS);
+    public GrantPermissionRule permissionRule2 = GrantPermissionRule.grant(Manifest.permission.POST_NOTIFICATIONS);
 
 
     @Rule
@@ -60,27 +66,55 @@ public class AttendeeViewTest {
     @Rule
     public ActivityScenarioRule<MainActivity> scenario = new
             ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+
+    private ViewIdlingResource idlingResource = new ViewIdlingResource(R.id.progress);
     @Test
     public void testchangeattendee(){
 
-        // click on attendee button
+        idlingResource.increment();
         onView(withId(R.id.attendeebtn)).perform(click());
-        // check if switches to attendee view
-        Espresso.onView(withId(R.id.progress)).check(matches(isDisplayed()));
+
+        // Wait for the progress bar to be displayed
+        onView(withId(R.id.progress)).check(matches(isDisplayed()));
+        // Check if the attendee view is displayed
         onView(withId(R.id.atten_view)).check(matches(isDisplayed()));
+
+        IdlingRegistry.getInstance().register(idlingResource);
+        idlingResource.decrement();
+        idlingResource.reset();
+        IdlingRegistry.getInstance().unregister(idlingResource);
+
+        onView(withId(R.id.atten_view)).check(matches(isDisplayed()));
+
+
+        IdlingRegistry.getInstance().unregister(idlingResource);
+
+
     }
 
     @Test
     public void testbackbutton(){
 
-        // click on attendee button
+        idlingResource.increment();
         onView(withId(R.id.attendeebtn)).perform(click());
 
+        // Wait for the progress bar to be displayed
+        onView(withId(R.id.progress)).check(matches(isDisplayed()));
+        onView(withId(R.id.atten_view)).check(matches(isDisplayed()));
+
+        IdlingRegistry.getInstance().register(idlingResource);
+        idlingResource.decrement();
+        idlingResource.reset();
+        IdlingRegistry.getInstance().unregister(idlingResource);
         // click on back button
         onView(withId(R.id.backbtn)).perform(click());
 
+
         // check if it goes to homepage
         onView(withId(R.id.main_activity_page)).check(matches(isDisplayed()));
+
+        IdlingRegistry.getInstance().unregister(idlingResource);
 
 
     }
@@ -108,34 +142,28 @@ public class AttendeeViewTest {
         onView(withId(R.id.eventname_text)).check(matches(withText("Test Event1")));
     }
 
-    @Test
-    public void testannouncements() {
 
-        onView(withId(R.id.attendeebtn)).perform(click());
-        // check if switches to attendee view
-        onView(withId(R.id.atten_view)).check(matches(isDisplayed()));
-
-        // click on event
-        onData(is(instanceOf(Event.class))).inAdapterView(withId(R.id.events)).perform(click());
-        // check if it switches to event details page
-        onView(withId(R.id.eventdet_frag)).check(matches(isDisplayed()));
-        // check if it shows name of event
-    }
 
     @Test
     public void testAnnouncementsList() {
+        idlingResource.increment();
+        onView(withId(R.id.attendeebtn)).perform(click());
+
+        // Wait for the progress bar to be displayed
+        onView(withId(R.id.progress)).check(matches(isDisplayed()));
+        onView(withId(R.id.atten_view)).check(matches(isDisplayed()));
+
+        IdlingRegistry.getInstance().register(idlingResource);
+        idlingResource.decrement();
+        idlingResource.reset();
+        IdlingRegistry.getInstance().unregister(idlingResource);
+
+        onView(withId(R.id.messages2)).perform(click());
+
         // Check if the announcements list is displayed
         onView(withId(R.id.announcements_list)).check(matches(isDisplayed()));
     }
 
-
-    public void testNavigationToAnnouncements() {
-        // Click on the "Announcements" item in the bottom navigation bar
-        onView(withId(R.id.messages2)).perform(click());
-
-        // Check if the Announcements fragment is displayed
-        onView(withId(R.id.announcements_list)).check(matches(isDisplayed()));
-    }
 
 
 

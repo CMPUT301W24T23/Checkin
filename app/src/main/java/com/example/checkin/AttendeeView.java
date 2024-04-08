ca80bc3999aead71d844c4ad9247088f1fd62c11
Samira Salman <samirasalman24@gmail.com>
2024-04-07 19:22:09 -0600

    fix moving to attendeeview

--- app/src/main/java/com/example/checkin/AttendeeView.java
+++ app/src/main/java/com/example/checkin/AttendeeView.java
@@ -131,6 +131,8 @@ public class AttendeeView extends AppCompatActivity {
             String qrCodeContent = intentResult.getContents();
             // retrive event related to scanned qr code, and check in attendee
             getEventDetailsFromFirebase(qrCodeContent, android_id);
+            BottomNavigationView bottomNavigationView2 = findViewById(R.id.bottomnavbar2);
+            bottomNavigationView2.setSelectedItemId(R.id.home2);
 
 
         }
